b2b17342be147f9e5d83b9b87f857d8d08414ef7
Bhavyajain1999 <155519416+Bhavyajain1999@users.noreply.github.com>
2024-01-19 03:17:54 +0000

    day 2

---
+++ operators.java
@@ -0,0 +1,17 @@
+//Assginment operator //
+public class operators {
+    public static void main(String[] args){
+        int myInt = 9; //assignment operator//
+        int newInt = 10; //assignment of new operator//
+        int exInt = myInt;
+        System.out.println(myInt);
+        System.out.println(newInt);
+        System.out.println(exInt);
+        Boolean chk = (myInt == exInt);
+        System.out.println(chk);
+
+
+
+
+    }
+}
