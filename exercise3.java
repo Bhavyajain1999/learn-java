6c193f656e020217128584188b1ccc6cabdd5fc6
Bhavyajain1999 <155519416+Bhavyajain1999@users.noreply.github.com>
2024-01-19 06:27:16 +0000

    exercise completed

---
+++ exercise3.java
@@ -0,0 +1,67 @@
+// number is positive, negative, zero
+// number is even or odd
+// greatest of three
+// year is leap year or not 
+// program to give grade from percentage
+// program to give age group on the basis of age
+
+import java.util.Scanner;
+
+public class exercise3 {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        
+        //  System.out.println("please enter number a ");
+        // int a = input.nextInt();
+        // if(a>0){System.out.println(" number is positive ");}
+        // else if(a<0){ System.out.println("number is negative");}
+        // else {System.out.println("it's zero");}
+
+        // System.out.println("please enter number a ");
+        // int a = input.nextInt();
+        // if((a%2)==0){System.out.println(" number is even");}
+        // else { System.out.println("number is odd");}
+      
+        // System.out.println("please enter number a ");
+        // int a = input.nextInt();
+        // System.out.println("please enter number b ");
+        // int b = input.nextInt();
+        // System.out.println("please enter number c ");
+        // int c = input.nextInt();
+        // if(a>b && a>c){System.out.println(" a is greatest ");}
+        // else if(a>b && a<c){ System.out.println(" c is greatest");}
+        // else if (b>a && b>c){System.out.println(" b is greatest ");}
+        // else if(b>a && b<c){ System.out.println(" c is greatest");}
+        // else if(c>b && c>a){System.out.println(" c is greatest ");}
+        // else if(c>b && c<a){ System.out.println(" a is greatest");}
+
+        // System.out.println("please enter year  ");
+        // int y = input.nextInt();
+        // if((y%4 == 0) && (y%400 == 0)){System.out.println(" it's a leap year");}
+        // else if((y%4 == 0) && (y%100 != 0))   { System.out.println(" it is leap year");}
+        // else{System.out.println("it's not a leap year");}
+
+        //  System.out.println("please enter percentage  ");
+        //  int p = input.nextInt();
+        // if((p > 90) ){System.out.println(" You have got A grade");}
+        // else if((p < 90) && (p > 75))  { System.out.println(" You have got B grade");}
+        // else if((p < 75) && (p > 60))  { System.out.println(" You have got C grade");}
+        // else if((p < 60) && (p > 30))  { System.out.println(" You have got D grade");}
+        // else if((p < 30))  { System.out.println(" You have got F grade");}
+
+        // System.out.println("please enter your age  ");
+        //  int age = input.nextInt();
+        // if((age > 60) ){System.out.println(" You are senior");}
+        // else if((age < 60) && (age > 20))  { System.out.println(" You are adult");}
+        // else if((age < 20) && (age > 13))  { System.out.println(" You are teenager");}
+        // else if((age < 13))  { System.out.println(" You are child");}
+        
+       
+       
+
+
+
+
+}
+    
+}
